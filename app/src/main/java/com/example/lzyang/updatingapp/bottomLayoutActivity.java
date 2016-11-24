6b893e88816676b43faff2d0eb2642978be15a63
LzYang <LzYang>
2016-11-25 00:32:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lzyang/updatingapp/bottomLayoutActivity.java
@@ -0,0 +1,28 @@
+package com.example.lzyang.updatingapp;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+
+public class bottomLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_layout);
+
+        TabHost host = (TabHost)findViewById(R.id.tabhost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.content_main);
+        spec.setIndicator("Tab One");
+        host.addTab(spec);
+    }
+
+
+
+}
