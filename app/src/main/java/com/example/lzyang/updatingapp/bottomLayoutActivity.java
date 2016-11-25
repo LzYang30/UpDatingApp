f8498afa1f52571e9b8b84158d47f5f11f1d4ef1
Lz-Yang <limzy-wa14@student.tarc.edu.my>
2016-11-26 00:54:29 +0800

    Added Bottom bar =D

--- app/src/main/java/com/example/lzyang/updatingapp/bottomLayoutActivity.java
+++ app/src/main/java/com/example/lzyang/updatingapp/bottomLayoutActivity.java
@@ -1,10 +1,10 @@
 package com.example.lzyang.updatingapp;
 
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TabHost;
-
+import android.widget.ImageView;
 
 public class bottomLayoutActivity extends AppCompatActivity {
 
@@ -12,17 +12,6 @@ public class bottomLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_layout);
-
-        TabHost host = (TabHost)findViewById(R.id.activity_bottom_layout);
-        host.setup();
-
-        //Tab 1
-        TabHost.TabSpec spec = host.newTabSpec("Tab One");
-        spec.setContent(R.id.content_main);
-        spec.setIndicator("Tab One");
-        host.addTab(spec);
     }
-
-
 
 }
